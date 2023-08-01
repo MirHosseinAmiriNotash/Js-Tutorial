e5ad4a94478e15208871e6cbab1729e677ae6181
Hossein <mh.amiri.notash@gmail.com>
2023-08-01 11:56:39 +0430

    Naming rules

--- 2-Variables/script.js
+++ 2-Variables/script.js
@@ -1,3 +1,28 @@
 
 let name = "Hossein";
 console.log(name);
+
+// let let = "Hossein";
+// console.log(let);
+
+// let if = "Hossein";
+// console.log(if);
+
+// let 1name = "Hossein";
+// console.log(1name);
+
+// let user name = "Hossein";
+// console.log(user name);
+
+// let user-name = "Hossein";
+// console.log(user-name);
+
+let userName = "Hossein";
+console.log(userName);
+
+// let username = "Hossein";
+// Console.log(username);
+// console.log(UserName);
+
+let fname = "Hossein" , lname = "Amiri";
+console.log(fname + lname);
