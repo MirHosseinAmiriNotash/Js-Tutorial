9a0d2e0b195cb16bb450fdcdb37b353d8523e3d3
Hossein <mh.amiri.notash@gmail.com>
2023-08-01 13:05:22 +0430

    Primary types

---
+++ 4-Primary types/script.js
@@ -0,0 +1,18 @@
+//primitive(value) types
+//reference types
+
+
+//primitive(value) types:
+//string
+//number
+//bool
+//undifined
+//null
+
+let name = "Hossein";
+let lname; //or let lname = undifined
+let gender = null;
+let age = 20;
+let admin = false;
+
+console.log(gender);
