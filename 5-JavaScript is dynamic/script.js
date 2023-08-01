979917bfd1bf5755325934db28806c249e9147e4
Hossein <mh.amiri.notash@gmail.com>
2023-08-01 13:46:08 +0430

    Dynamic JavaScript was tested

---
+++ 5-JavaScript is dynamic/script.js
@@ -0,0 +1,19 @@
+
+let name = "ali";
+console.log(name);
+console.log(typeof name);//string
+
+name = 25;
+console.log(typeof name);//number
+
+name = 15.5;
+console.log(typeof name);
+
+let admin = true;
+console.log(typeof admin);//boolean
+
+let fname;
+console.log(typeof fname);//undefined
+
+let product = null;
+console.log(typeof product);//object
