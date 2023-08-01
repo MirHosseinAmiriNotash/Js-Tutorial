fd1d9f912e7995977c85d5f63d9431c6e883b486
Hossein <mh.amiri.notash@gmail.com>
2023-08-01 12:24:26 +0430

    Create variable with const

---
+++ 3-Constants/script.js
@@ -0,0 +1,15 @@
+
+// let name = "Hossein";
+// let name = "reza";
+
+// console.log(name);
+
+let Name = "Hossein";
+Name = "reza";
+Name = "ali";
+
+console.log(Name);
+
+const name = "Hosssein";
+name    = ali;
+console.log(name);
