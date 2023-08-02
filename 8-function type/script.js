2883bf8ac37d4a15c4f7de17a8c853169d57ba53
Hossein <mh.amiri.notash@gmail.com>
2023-08-02 23:06:52 +0430

    Create function

---
+++ 8-function type/script.js
@@ -0,0 +1,17 @@
+
+function greet(name , age){
+    let calc = 7 + 7;
+    console.log("Hello World!");
+    console.log("Hello " + name + " your are " + age + " years old");
+    console.log(calc);
+    console.log("name: " + name + "  age: " + age);
+}
+
+function square(number){
+    return number * number;
+}
+
+greet("Hossein" , 20);
+
+let result = square(7);
+console.log(result);
