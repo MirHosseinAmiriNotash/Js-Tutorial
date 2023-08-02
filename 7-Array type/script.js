65559590322e3b543bbef547e725061d6083f5e7
Hossein <mh.amiri.notash@gmail.com>
2023-08-02 15:34:56 +0430

    Increase the length of the array

--- 7-Array type/script.js
+++ 7-Array type/script.js
@@ -1,6 +1,11 @@
 
 let users = ["Hossein" , "sam"];
-console.log(users);
+console.log(users);//['Hossein', 'sam']
 
 
-console.log(users[1]);
+console.log(users[1]);//sam
+console.log(users[0]);//Hossein
+
+
+users[2] = "ali";
+console.log(users);//['Hossein', 'sam', 'ali']
