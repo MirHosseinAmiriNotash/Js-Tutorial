e521374eb7c9ddc3ebace4546305d3c276c06411
Hossein <mh.amiri.notash@gmail.com>
2023-08-02 14:52:02 +0430

    Create Object

---
+++ 6-Object type/script.js
@@ -0,0 +1,30 @@
+//Reference Types
+//object
+//Array
+//Function
+
+// let name = "Hossein";
+// let age = 20;
+// let admin = true;
+
+let person = {
+    name : "Hossein" ,
+    age : 20 ,
+    admin : true ,
+}
+
+console.log(person);
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.admin);
+
+console.log(person["name"]);
+console.log(person["age"]);
+console.log(person["admin"]);
+
+console.log(person.name , person.age);
+console.log(person["name"] , person["admin"]);
+
+
+
