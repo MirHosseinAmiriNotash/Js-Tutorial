4a089bf0475169de0138c66e1a101a8f7209654d
Hossein <mh.amiri.notash@gmail.com>
2023-08-03 12:40:56 +0430

    use Comparison operators

---
+++ 11-Comparison operators/script.js
@@ -0,0 +1,23 @@
+//relational
+//> < >= <=
+let x = 1;
+console.log(x > 1);
+console.log(x < 1);
+console.log(x >= 1);
+console.log(x <= 1);
+
+//equality
+//lose equality
+// == !=
+//strict equality
+// === !==
+
+console.log(x == 1);
+console.log(x != 1);
+
+console.log('1' == 1);//true(convert string to number)
+console.log(true == 1);//true(convert 1 to true)
+console.log('1' === 1);//false
+console.log('1' !== 1);//true
+
+
