ce8b97d7a9cd61d4c3e2eca9d8f6d8098bd3ae0d
Hossein <mh.amiri.notash@gmail.com>
2023-08-06 10:49:19 +0430

    use if-else

---
+++ 17-if-else conditional statement/script.js
@@ -0,0 +1,13 @@
+// 6am - 12pm -> good morning
+// 12pm - 6pm -> good afternoon
+// otherwise -> good evening
+
+let hour = 20;
+
+if(hour>=6 && hour<12){
+    console.log("good morning");
+}else if(hour>=12 && hour<18){
+    console.log("good afternoon");
+} else{
+    console.log("good evening");
+}
