83216711af6047eb4c4d89b080f1cdca10430e81
Hossein <mh.amiri.notash@gmail.com>
2023-08-07 14:19:57 +0430

    switch-case

---
+++ 18- switch-case/script.js
@@ -0,0 +1,21 @@
+
+let role = "admin";
+
+switch (role) {
+    case "guest":
+        console.log("guest user");
+        break;
+    case "admin":
+        console.log("admin user");
+        break;
+    default:
+        console.log("unknown user");
+}
+
+// if(role === "guest"){
+//     console.log("guest user");
+// }else if(role === "admin"){
+//     console.log("admin user");
+// }else{
+//     console.log("unknown user");
+// }
