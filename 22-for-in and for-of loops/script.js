76394fa729a64ebe0e4fb53dba3690897411aef4
Hossein <mh.amiri.notash@gmail.com>
2023-08-07 21:32:21 +0430

    for-in and for-of loops

---
+++ 22-for-in and for-of loops/script.js
@@ -0,0 +1,17 @@
+
+const person = {name:"Hossein" , age: 20};
+const colors = ['red' , 'green', 'blue'];
+
+// for(let key in person){
+//     console.log(key);
+//     console.log(person[key]);
+// }
+
+// for(let index in colors){
+//     console.log(index);
+//     console.log(colors[index]);
+// }
+
+for(let color of colors){
+    console.log(color);
+}
