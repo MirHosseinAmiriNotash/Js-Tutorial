45f0f388ae3a4567f043860e48428ebd8f045592
Hossein <mh.amiri.notash@gmail.com>
2023-08-07 21:56:30 +0430

    break and continue

---
+++ 23-break and continue/script.js
@@ -0,0 +1,19 @@
+
+let i = 0;
+
+// while(i<=10){
+//     if(i == 5){
+//         break;
+//     }
+//    console.log(i);
+//    i++;
+// }
+
+while(i<=10){
+    if(i == 5){
+        i++;
+        continue;
+    }
+   console.log(i);
+   i++;
+}
