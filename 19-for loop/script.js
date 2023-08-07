8439b4105cc53c79bbf393f822c0904fb7a6356a
Hossein <mh.amiri.notash@gmail.com>
2023-08-07 14:45:20 +0430

    for loop

---
+++ 19-for loop/script.js
@@ -0,0 +1,23 @@
+
+//for
+//while
+//do while
+//for .. in
+//for .. out
+
+// for(let i = 0;i < 5;i++){
+//     console.log("Hossein");
+//     console.log(i);
+// }
+
+// for(let i = 0;i < 50;i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+for(let i = 50;i > 1;i--){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
