b4fe6134397d6e384a52cc77e89798bbcdd00088
Hossein <mh.amiri.notash@gmail.com>
2023-08-07 23:00:22 +0430

    Objects

---
+++ 24-Objects/script.js
@@ -0,0 +1,14 @@
+
+function draw(){}
+
+let circle = {
+    radius : 1 ,
+    location : {
+        x:1,
+        y:1,
+    },
+    draw: function(){console.log("draw")} ,
+    inVisible: true
+}
+
+circle.draw();
