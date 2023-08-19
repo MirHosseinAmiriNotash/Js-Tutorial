c9d71b9f356b186f4ee682024f8041380eec4636
Hossein <mh.amiri.notash@gmail.com>
2023-08-19 15:27:28 +0430

    Literal template

---
+++ 35-Literal template/script.js
@@ -0,0 +1,34 @@
+// const message = '\'Hossein\' . \n' + 
+// ' coding is cool';
+
+// {}
+// true , false
+// ' ""
+// `
+
+// const another = `Hossein .
+// coding is cool
+// `;
+
+// console.log(another);
+
+// const another2 = `'Hossein' .
+// coding is cool
+// `;
+
+// console.log(another2);
+
+// let name = 'Hossein';
+// let msg = 'hi ' + name + ' . thank you';
+
+// console.log(msg);//hi Hossein. thank you
+
+let name = 'Hossein';
+let msg = `hi ${name}. thank you`;
+
+console.log(msg);//hi Hossein. thank you
+
+// let name = 'Hossein';
+// let msg = `hi ${name}. thank you`;
+
+// console.log(msg);//hi Hossein. thank you
