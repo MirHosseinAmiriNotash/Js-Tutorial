65dec2448f50967304017a10cad5b5cd33ec02a7
Hossein <mh.amiri.notash@gmail.com>
2023-08-26 14:55:28 +0430

    Array concatenation

---
+++ 45-Array concatenation/script.js
@@ -0,0 +1,25 @@
+const numbers  = [1,2,3]; 
+
+// const joined = numbers.join();
+// console.log(joined);// 1,2,3
+
+// const joined = numbers.join(' ');
+// console.log(joined);// 1 2 3
+
+// const joined = numbers.join('');
+// console.log(joined);// 123
+
+// console.log(typeof joined);// string
+
+const message = "Neon coding is cool";
+const parts = message.split(' ');
+console.log(parts);// ['Neon', 'coding', 'is', 'cool']
+const combined = parts.join('-');
+console.log(combined);//Neon-coding-is-cool
+
+
+// const message2 = "Neon coding , is cool";
+// const parts2 = message2.split(',');
+// console.log(parts2);// ['Neon coding ', ' is cool']
+
+//neonlearn.ir/article/Neon-coding-is-cool
