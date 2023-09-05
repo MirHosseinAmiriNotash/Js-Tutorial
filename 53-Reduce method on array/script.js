6dcfc959f493a57f685705660afa8a4d7979696d
Hossein <mh.amiri.notash@gmail.com>
2023-09-05 14:13:41 +0430

    Reduce method on array

---
+++ 53-Reduce method on array/script.js
@@ -0,0 +1,32 @@
+const numbers = [1,-1,2,3];
+
+// let sum = 0;
+// for(let n of numbers){
+//     sum += n;
+// }
+
+// console.log(sum);// 5
+
+let sum = numbers.reduce(function(accumulator , currentValue){
+   return accumulator + currentValue;
+} , 0);
+
+console.log(sum);// 5
+
+
+// a = 0 , c = 1 => a = 1
+// a = 1 , c = -1 => a = 0
+// a = 0 , c = 2 => a = 2
+// a = 2 , c = 3 => a = 5
+// let sum = 5 ;
+
+let sum2 = numbers.reduce(function(accumulator , currentValue){
+    return accumulator + currentValue;
+ });
+ 
+ console.log(sum2);// 5
+
+ // a = 1 , c = -1 => a = 0
+// a = 0 , c = 2 => a = 2
+// a = 2 , c = 3 => a = 5
+// let sum = 5 ;
