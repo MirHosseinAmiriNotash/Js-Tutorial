8ab5a8ea9403a7dde8e77cf36995d3b770310fad
Hossein <mh.amiri.notash@gmail.com>
2023-09-06 22:50:59 +0430

    Variable definition with var

---
+++ 58-Variable definition with var/script.js
@@ -0,0 +1,31 @@
+// var y = 0;
+
+// function start(){
+//     for( let i = 0; i<5 ; i++){
+//         console.log(i);
+//     }
+//     console.log(i);// error - script.js:7 Uncaught ReferenceError: i is not defined at start (script.js:7:17)
+// }
+
+
+// start();
+
+// function start(){
+//     for( var i = 0; i<5 ; i++){
+//         console.log(i);
+//     }
+//     console.log(i);// error - script.js:7 Uncaught ReferenceError: i is not defined at start (script.js:7:17)
+// }
+
+// // var => function-scoped
+// // let,const => block-scoped
+
+// start();
+
+
+// var color = 'red';
+// let age = 20; 
+
+function sayHi(){
+    console.log('hi');
+}
