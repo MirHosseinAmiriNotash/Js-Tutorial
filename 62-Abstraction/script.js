f00e7206e0dcf4b1ea6a9407704abdf00978ec07
Hossein <mh.amiri.notash@gmail.com>
2023-09-12 15:39:59 +0430

    Abstraction

---
+++ 62-Abstraction/script.js
@@ -0,0 +1,14 @@
+function Circle(radius){
+    this.radius = radius;
+    let defaultLocation = {x : 0 , y : 0};
+    let computeOptimumlocation = function(factor){
+        //...
+    }
+    this.draw = function(){
+        computeOptimumlocation();
+        console.log('draw');
+    }
+    
+}
+
+const circle = new Circle(10);
