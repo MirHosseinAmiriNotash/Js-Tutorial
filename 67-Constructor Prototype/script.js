7b3d52c8e202e244ddfda5a64794db3b5c085313
Hossein <mh.amiri.notash@gmail.com>
2023-09-18 13:33:07 +0430

    Constructor Prototype

---
+++ 67-Constructor Prototype/script.js
@@ -0,0 +1,13 @@
+function Circle(radius){
+    this.radius = radius;
+}
+
+const circle = new Circle(1);
+
+// Object.getPrototypeOf(Circle);
+// Circle.__proto__;
+
+
+// Circle.prototype
+
+//circle.__proto__ = Circle.prototype
