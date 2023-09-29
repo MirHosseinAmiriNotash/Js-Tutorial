798456de1c609d2073da95a840d0c955e63546f7
Hossein <mh.amiri.notash@gmail.com>
2023-09-29 20:56:56 +0330

    Hoisting in class

---
+++ 77-Hoisting in class/script.js
@@ -0,0 +1,26 @@
+
+// sayHello();
+// sayGoodbye();
+// // Function Declaration
+// function sayHello(){
+//     console.log('Hello');
+// }
+
+
+// // Function Expression
+// const sayGoodbye = function(){
+//     console.log('Goodbye');
+// }
+
+// const c = new Circle();
+const s = new Square();
+// Class Declaration
+class Circle{
+
+}
+
+
+// class Expression
+const Square = class{
+         
+}
