98fac837a405d64f88b0578bf6d6ef7c3009d1d1
Hossein <mh.amiri.notash@gmail.com>
2023-09-29 20:19:50 +0330

    Classes

---
+++ 76-Classes/script.js
@@ -0,0 +1,23 @@
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+// }
+
+
+
+class Circle{
+    constructor(radius){
+       this.radius = radius;
+       this.move  = function(){
+        console.log('move');
+       }
+    }
+    
+    draw(){
+        console.log('draw');
+    }
+}
+
+const c = new Circle(1);
