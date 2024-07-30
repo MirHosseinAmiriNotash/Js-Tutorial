2fd7cd0c87afafc7894cf8cda76052ff133f1d4a
Hossein <mh.amiri.notash@gmail.com>
2024-07-30 23:42:42 +0330

    vite

---
+++ 90-Vite/my-js-app/main.js
@@ -0,0 +1,5 @@
+import { TestMatch as Mathdsdsdsad} from "./src/js/Match";
+import LogDATA from "./src/js/log"
+const Match = new Mathdsdsdsad();
+Match.Add(5 , 4);
+LogDATA();
---
+++ 90-Vite/my-js-app/src/js/Match.js
@@ -0,0 +1,5 @@
+export class TestMatch{
+    Add(a , b){
+        console.log(a+b);
+    }
+}
